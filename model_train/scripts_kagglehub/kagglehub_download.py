823dd8a504cd25d824c6e27dec8acdeec0690b89
MrGiDmans <tumambae@gmail.com>
2025-04-15 20:49:17 +0500

    Изменил README.md и исправил скрипт kagglehub_download.py

--- model_train/scripts_kagglehub/kagglehub_download.py
+++ model_train/scripts_kagglehub/kagglehub_download.py
@@ -1,13 +1,17 @@
 import kagglehub
 import os
 
-# Укажите путь к директории, в которую хотите сохранить датасет
-save_path = "./data"  # Замените на нужный путь
+def main():
+    # Укажите путь к директории, в которую хотите сохранить датасет
+    save_path = "./data1"  # Замените на нужный путь
 
-# Создайте директорию, если она не существует
-os.makedirs(save_path, exist_ok=True)
+    # Создайте директорию, если она не существует
+    os.makedirs(save_path, exist_ok=True)
 
-# Загрузите датасет, указав путь сохранения
-path = kagglehub.dataset_download("agrigorev/dino-or-dragon", destdir=save_path)
+    # Загрузите датасет, указав путь сохранения
+    path = kagglehub.dataset_download("agrigorev/dino-or-dragon", path=save_path)
 
-print("Путь к файлам датасета:", path)
+    print("Путь к файлам датасета:", path)
+
+if __name__ == "__main__":
+    main()
